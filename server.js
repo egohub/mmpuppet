db84bed7f2a197a1f88a79d3a246dba78fde383a
ego <ego@hackermail.com>
2019-11-30 03:56:17 +0630

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app     = express();
+
+const routes = require('./routes');
+app.use('/', routes);
+// app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+app.get("/", (req, res) => {
+  res.send("This is the Movie API. https://channelmyanmar.org/line-of-duty2019/ mmpuppet.herokuapp.com/movie/line-of-duty2019");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log('The server has started on port', port);
+});
