57933ab84d628f848637d430d6d1c98e2843fc65
ego <ego@hackermail.com>
2019-11-30 05:27:16 +0630

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const Scraper = require('./controller/scraper.js');
 
 router.get('/movie/:id', Scraper.scrapeMovie);
+router.get('/movie', Scraper.scrapeMovies);
 
 module.exports = router;
