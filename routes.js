9c685cc8839a80852259b818648e69bbf296b610
ego <ego@hackermail.com>
2020-07-21 07:05:10 +0630

    Update routes.js

--- routes.js
+++ routes.js
@@ -5,5 +5,7 @@ const Scraper = require('./controller/scraper.js');
 router.get('/movie/:id', Scraper.scrapeMovie);
 router.get('/movie', Scraper.scrapeMovies);
 router.get('/odd', Scraper.scrapeOdd);
+router.get('/odd', Scraper.scrapeOdd);
+router.get('/stream', Scraper.stream);
 
 module.exports = router;
